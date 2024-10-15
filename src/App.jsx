376111690c315454b0b790fd15ae3435f47f53de
Avivdo7 <55305605+Avivdo7@users.noreply.github.com>
2024-10-15 19:30:03 +0300

    Adjusted Sticky Navbar, added meta data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Helmet } from "react-helmet";
 import StickyNavbar from "./components/StickyNavbar";
 import Hero from "./components/Hero";
 import Tech from "./components/Tech";
@@ -7,23 +8,46 @@ import Exp from "./components/Exp";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-
 const App = () => {
   return (
-    <div className="overflow-x-hidden overflow-y-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      </div>
+    <>
+      <Helmet>
+        <title>Portfolio | My Projects</title>
+        <meta
+          name="description"
+          content="Browse through my portfolio projects and skills."
+        />
+        <meta property="og:title" content="My Portfolio" />
+        <meta
+          property="og:description"
+          content="Explore projects and skills in my portfolio."
+        />
+      </Helmet>
+      <div className="overflow-x-hidden overflow-y-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+        <div className="fixed top-0 -z-10 h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        </div>
 
-      <div className="container mx-auto px-8">
-        <StickyNavbar />
-        <div id="hero"><Hero /></div>
-        <div id="exp"><Exp /></div>
-        <div id="tech"><Tech /></div>
-        <div id="projects"><Projects /></div>
-        <div id="contact"><Contact /></div>
+        <div className="container mx-auto px-8">
+          <StickyNavbar />
+          <div id="hero">
+            <Hero />
+          </div>
+          <div id="exp">
+            <Exp />
+          </div>
+          <div id="tech">
+            <Tech />
+          </div>
+          <div id="projects">
+            <Projects />
+          </div>
+          <div id="contact">
+            <Contact />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
